1c9e67ec33e9ee52ac124c8ef909fd5cb4975148
chickendude <pokemaster103@gmail.com>
2017-05-01 18:07:13 +0800

    Fixed users getting displayed twice in friends fragment.

--- app/src/main/java/com/teamtreehouse/ribbit/adapters/MessageAdapter.java
+++ app/src/main/java/com/teamtreehouse/ribbit/adapters/MessageAdapter.java
@@ -18,64 +18,66 @@ import java.util.List;
 
 public class MessageAdapter extends ArrayAdapter<Message> {
 
-    protected Context mContext;
-    protected List<Message> mMessages;
+	private static final String TAG = MessageAdapter.class.getSimpleName();
+	protected Context mContext;
+	protected List<Message> mMessages;
 
-    public MessageAdapter(Context context, List<Message> messages) {
-        super(context, R.layout.message_item, messages);
-        mContext = context;
+	public MessageAdapter(Context context, List<Message> messages) {
+		super(context, R.layout.message_item, messages);
+		mContext = context;
 
-        // Create a full copy of mMessages
-        mMessages = new ArrayList<Message>();
-        for (Message msg : messages) {
-            mMessages.add(msg);
-        }
-    }
+		// Create a full copy of mMessages
+		mMessages = new ArrayList<Message>();
+		for (Message msg : messages) {
+			mMessages.add(msg);
+		}
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
 
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
-            holder = new ViewHolder();
-            holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
-            holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
-            holder.timeLabel = (TextView) convertView.findViewById(R.id.timeLabel);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
+			holder = new ViewHolder();
+			holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
+			holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
+			holder.timeLabel = (TextView) convertView.findViewById(R.id.timeLabel);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
 
-        Message message = mMessages.get(position);
+		Message message = mMessages.get(position);
 
-        Date createdAt = message.getCreatedAt();
-        long now = new Date().getTime();
-        SimpleDateFormat format = new SimpleDateFormat("EEE, MMM d");
-        String convertedDate = format.format(createdAt);
+		Date createdAt = message.getCreatedAt();
+		long now = new Date().getTime();
+		SimpleDateFormat format = new SimpleDateFormat("EEE, MMM d");
+		String convertedDate = format.format(createdAt);
 
-        holder.timeLabel.setText(convertedDate);
+		holder.timeLabel.setText(convertedDate);
 
-        if (message.getString(Message.KEY_FILE_TYPE).equals(Message.TYPE_IMAGE)) {
-            holder.iconImageView.setImageResource(R.drawable.ic_picture);
-        } else {
-            holder.iconImageView.setImageResource(R.drawable.ic_video);
-        }
-        holder.nameLabel.setText(message.getString(Message.KEY_SENDER_NAME));
+		if (message.getString(Message.KEY_FILE_TYPE).equals(Message.TYPE_IMAGE)) {
+			holder.iconImageView.setImageResource(R.drawable.ic_picture);
+		} else {
+			holder.iconImageView.setImageResource(R.drawable.ic_video);
+		}
+		holder.nameLabel.setText(message.getString(Message.KEY_SENDER_NAME));
 
-        return convertView;
-    }
+		return convertView;
+	}
 
-    private static class ViewHolder {
-        ImageView iconImageView;
-        TextView nameLabel;
-        TextView timeLabel;
-    }
+	private static class ViewHolder {
+		ImageView iconImageView;
+		TextView nameLabel;
+		TextView timeLabel;
+	}
 
-    public void refill(List<Message> messages) {
-        mMessages.clear();
-        mMessages.addAll(messages);
-        notifyDataSetChanged();
-    }
+	public void refill(List<Message> messages) {
+		mMessages.clear();
+		mMessages.addAll(messages);
+		notifyDataSetChanged();
+	}
 }
 
 

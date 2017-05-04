cbdc91ff8d9655f4c61a81b4bd284aacfbd673b6
chickendude <pokemaster103@gmail.com>
2017-05-05 01:46:10 +0800

    Added counter to image page as well.

--- app/src/main/java/com/teamtreehouse/ribbit/ui/ViewImageActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/ui/ViewImageActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.teamtreehouse.ribbit.R;
@@ -15,50 +16,67 @@ import java.util.TimerTask;
 
 public class ViewImageActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_image);
-        // Show the Up button in the action bar.
-        setupActionBar();
+	private int mCounter;
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_image);
+		// Show the Up button in the action bar.
+		setupActionBar();
 
-        Uri imageUri = getIntent().getData();
+		mCounter = 10;
 
-        Picasso.with(this).load(imageUri.toString()).into(imageView);
+		final TextView counterLabel = (TextView) findViewById(R.id.counterLabel);
+		counterLabel.setText(String.format("Message will be destroyed in %s seconds", mCounter));
 
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                finish();
-            }
-        }, 10 * 1000);
-    }
+		ImageView imageView = (ImageView) findViewById(R.id.imageView);
+		Uri imageUri = getIntent().getData();
+		Picasso.with(this).load(imageUri.toString()).into(imageView);
 
-    /**
-     * Set up the {@link android.app.ActionBar}.
-     */
-    private void setupActionBar() {
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				mCounter--;
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						counterLabel.setText(String.format("Message will be destroyed in %s seconds", mCounter));
+					}
+				});
+			}
+		}, 1000, 1000);
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				finish();
+			}
+		}, 10 * 1000);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				// This ID represents the Home or Up button. In the case of this
+				// activity, the Up button is shown. Use NavUtils to allow users
+				// to navigate up one level in the application structure. For
+				// more details, see the Navigation pattern on Android Design:
+				//
+				// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+				//
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }

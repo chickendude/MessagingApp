71312004cc73bd924af404947a197f0ea617c169
chickendude <pokemaster103@gmail.com>
2017-05-07 20:24:30 +0800

    Moved requestWindowFeature to top of onCreate.

--- app/src/main/java/com/teamtreehouse/ribbit/ui/EditFriendsActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/ui/EditFriendsActivity.java
@@ -36,8 +36,8 @@ public class EditFriendsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.user_grid);
         // Show the Up button in the action bar.
         setupActionBar();
